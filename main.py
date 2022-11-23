76ab6ef5d2e9fcc7a1d2fda9c99861c0b399374e
resplit <67502036+aragha@users.noreply.github.com>
2022-11-23 03:44:27 +0000

    moved the calculator code into a while loop to allow user to do repeat calculations

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ def divide(n1, n2):
 
 # create a dictionary to store the operations and their functions
 operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
-
-num1 = int(input("Enter your first number : "))
-for symbol in operations:
-  print(symbol)
-operation_symbol = input("Pick an operation from the above: ")
-num2 = int(input("Enter your second number: "))
-
-calc_function = operations[operation_symbol]
-answer = calc_function(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+proceed = 'y'
+while proceed == 'y':
+  num1 = int(input("Enter your first number : "))
+  for symbol in operations:
+    print(symbol)
+  operation_symbol = input("Pick an operation from the above: ")
+  num2 = int(input("Enter your second number: "))
+  
+  calc_function = operations[operation_symbol]
+  answer = calc_function(num1, num2)
+  print(f"{num1} {operation_symbol} {num2} = {answer}")
+  proceed = input("Would you like to do further calculations? say y/n :")
+  
